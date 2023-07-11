ddad8f7fb11880de433d9b40748995ce3a1b6ea5
edgardomendoza1g <edgardo.mendoza1g@gmail.com>
2023-07-11 23:37:52 +0000

    inside both components we are fetching the data from the api making use of our getPokemon functions, we also are passing the id by splitting  form the url string, this to our ppokemonCard component so we can acces every id pokemon

--- poke-app/src/components/PokemonCard.tsx
+++ poke-app/src/components/PokemonCard.tsx
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { CardContent, Typography, CardMedia } from "@mui/material";
 import { CardContainer, ContentContainer, Title } from "../styles/theme";
+import { PokemonDetail, PokemonProps } from "../types";
 
-const PokemonCard: React.FC<any> = () => {
+const PokemonCard: React.FC<PokemonProps> = ({ pokemon }) => {
+  const [pokemonData, setPokemonData] = useState<PokemonDetail>();
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        const response = await fetch(pokemon.url);
+        const data: PokemonDetail = await response.json();
+        setPokemonData(data);
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    };
+
+    fetchPokemonData();
+  }, [pokemon.url]);
+
+  const frontSprite = pokemonData?.sprites.other.dream_world.front_default;
+
   return (
     <CardContainer>
       <CardMedia
         component="img"
-       
+        src={frontSprite}
         style={{
           width: "150px",
           height: "150px",
@@ -19,10 +37,14 @@ const PokemonCard: React.FC<any> = () => {
         <ContentContainer>
           <Typography variant="subtitle1"># 1</Typography>
           <Typography component="div" variant="h5">
-            Charizard
+            {pokemon.name}
           </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-           Name
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
+            Name
           </Typography>
         </ContentContainer>
       </CardContent>
--- poke-app/src/pages/Home.tsx
+++ poke-app/src/pages/Home.tsx
@@ -1,16 +1,40 @@
-import { Grid } from "@mui/material";
-import React from "react";
+import { Box, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "../components/PokemonCard";
+import { useSearchParams } from "react-router-dom";
+import { pokeAPI } from "../services/pokeAPI";
 
 const Home = () => {
+  const [pokemonList, setPokemonList] = useState<any[]>();
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      try {
+        const data = await pokeAPI.getPokemonList(setPokemonList);
+        setPokemonList(data);
+      } catch (error) {
+        console.error("Error fetching Pokemon list:", error);
+        setPokemonList([]);
+      }
+    };
+
+    fetchPokemonList();
+  }, []);
+
   return (
-    <div>
+    <Box>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <PokemonCard />
-        </Grid>
+        <Box>
+          {pokemonList?.map((pokemon) => {
+            const id = pokemon.url.split("/")[6];
+            return (
+              <Grid>
+                <PokemonCard pokemon={pokemon} />
+              </Grid>
+            );
+          })}
+        </Box>
       </Grid>
-    </div>
+    </Box>
   );
 };
 
