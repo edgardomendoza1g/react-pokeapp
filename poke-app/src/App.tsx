a523591facde447a89ec73ddecb0eacae99ff5ae
edgardomendoza1g <edgardo.mendoza1g@gmail.com>
2023-07-11 22:30:47 +0000

    Routing our Home and our Pokemon details page

--- poke-app/src/App.tsx
+++ poke-app/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Pokemon from "./pages/Pokemon";
 
-
-function App() {
+const App: React.FC = () => {
   return (
-   <div></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon:id" element={<Pokemon />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
