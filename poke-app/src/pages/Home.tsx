d9716d043d202638327223d1f8668c0c93f35b14
edgardomendoza1g <edgardo.mendoza1g@gmail.com>
2023-07-11 22:38:04 +0000

    Create the PokemonCard component and we pass the component inside Home, right it just minimum code but here we have our basic structure, also we are using material UI to make it responsive.

--- poke-app/src/pages/Home.tsx
+++ poke-app/src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
+import PokemonCard from "../components/PokemonCard";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <PokemonCard />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
